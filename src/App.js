9992e9558fd488eb0cb5b96b962f4ac385be7ea2
EmmanuelBazan <emanuel.bazan@track.design>
2022-06-04 13:31:07 -0500

    Intent to hide scrollbar in firefox

--- src/App.js
+++ src/App.js
@@ -6,26 +6,18 @@ import Skills from './components/Skills/Skills';
 import Portafolio from './components/Portafolio/Portafolio';
 import Experience from './components/Experience/Experience';
 import Contact from './components/Contact/Contact';
-import { useRef } from 'react';
 
 function App() {
-
-  const homeRef = useRef()
-  const aboutMeRef = useRef()
-  const skillsRef = useRef()
-  const portafilioRef = useRef()
-  const experienceRef = useRef()
-  const contactRef = useRef()
 
   return (
     <>
       <NavBar></NavBar>
-      <Home ref={homeRef}></Home>
-      <AboutMe ref={aboutMeRef}></AboutMe>
-      <Skills ref={skillsRef}></Skills>
-      <Portafolio ref={portafilioRef}></Portafolio>
-      <Experience ref={experienceRef}></Experience>
-      <Contact ref={contactRef}></Contact>
+      <Home></Home>
+      <AboutMe></AboutMe>
+      <Skills></Skills>
+      <Portafolio></Portafolio>
+      <Experience></Experience>
+      <Contact></Contact>
     </>
   );
 }
--- src/app.elements.js
+++ src/app.elements.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
-export const ParentScroll = styled.div`
+export const ParentDiv = styled.div`
     width: 100%;
     height: 100%;
-    overflow: hidden;
+    overflow-x: hidden;
 `;
