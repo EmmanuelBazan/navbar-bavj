3288de1a403bca497e34a9ed3811aaa1935d0a77
EmmanuelBazan <emmanuelbazan99@gmail.com>
2025-04-13 16:43:12 -0600

    Update profil photo

--- src/components/Home/style/AboutMeCard.elements.js
+++ src/components/Home/style/AboutMeCard.elements.js
@@ -1,5 +1,6 @@
 import styled, { keyframes } from 'styled-components';
 import {Bookstack} from '@styled-icons/simple-icons/Bookstack'
+import profileImg from "../../../assets/profil-portafolio.jpg";
 
 const backgroundColorAnimation = keyframes`
     0% {
@@ -45,7 +46,7 @@ export const SubTitle = styled.text`
 export const AboutMeContainer = styled.div`
     display: flex;
     background-color: #f1f1f1;
-    justify-content: flex-end;
+    justify-content: flex-start;
     flex-direction: column;
     width: 100%;
     height: 80%;
@@ -56,7 +57,8 @@ export const AboutMeContainer = styled.div`
     /* animation: none; */
     /* transition: background-color 0.2s ease; */
 
-    background-image: url('https://i.pinimg.com/564x/56/dc/f3/56dcf3ebaa545146695ee13371c5fb7a.jpg'); /* Imagen de fondo */
+    /* background-image: url('https://i.pinimg.com/564x/56/dc/f3/56dcf3ebaa545146695ee13371c5fb7a.jpg'); Imagen de fondo */
+    background-image: url(${profileImg});
     background-size: cover; /* Ajusta la imagen para cubrir completamente el contenedor */
     background-position: center; /* Centra la imagen en el contenedor */
     background-repeat: no-repeat; /* Evita que la imagen se repita */
